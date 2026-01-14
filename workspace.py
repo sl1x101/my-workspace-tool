ceae727af349667836057dd59ef5d433f13fa64d
sxll_40 <rew2540@gmail.com>
2026-01-14 20:21:21 +0900

    refactor: improve openApp and organize URLs

--- workspace.py
+++ workspace.py
@@ -1,8 +1,31 @@
 import webbrowser
+import os
+import time
+
+urls =[
+        "https://www.google.com/",
+        "https://pomofocus.io/",
+        "https://open.spotify.com/"
+    ]
 
 #openweb 
 def openWeb():
-    webbrowser.open("https://www.google.com/")
-    webbrowser.open("https://pomofocus.io/")
-    webbrowser.open("https://open.spotify.com/") 
-openWeb()
+    for url in urls :
+        print("Open Web....")
+        webbrowser.open(url)
+        time.sleep(1)
+        
+#open app
+def openApp():
+    print("open app...")
+    try:
+        os.startfile("notepad.exe")
+        time.sleep(1)
+    except Exception as e:
+        print(f"Error:{e}")
+        
+if __name__ == "__main__":
+    print("Starting your Workspace...")
+    openWeb()
+    openApp()
+    print("Redy!..")
