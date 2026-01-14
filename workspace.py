89c8677e8f2bb6059dee41ab846c200a8f59e6fd
sxll_40 <rew2540@gmail.com>
2026-01-14 19:51:19 +0900

    First commit

---
+++ workspace.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+#openweb 
+def openWeb():
+    webbrowser.open("https://www.google.com/")
+    webbrowser.open("https://pomofocus.io/")
+    webbrowser.open("https://open.spotify.com/") 
+openWeb()
